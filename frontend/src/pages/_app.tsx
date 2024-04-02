6b9ca574dd37e7bc4db84e683f3f9ef998e94cb8
morningstar <mandeep7yadav@gmail.com>
2024-04-02 22:59:12 +0530

    navbar done

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,6 @@
+import Navbar from "@/Components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Navbar from "./components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
 
