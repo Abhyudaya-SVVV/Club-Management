cbea62e7b19aaff97927abbda997b9c251bf2a60
Abhyudaya-SVVV <165296600+Abhyudaya-SVVV@users.noreply.github.com>
2024-04-05 19:38:03 +0530

    Revert "this is just setup and this contains all the struture of file"

--- backend/src/utils/AsyncHandler.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = ( requrestHanderler) =>{
-    return ( req, res, next) => {
-        Promise
-        .resolve( requrestHanderler( req,res,next))
-        .catch((err) => next(err))
-    }    
-}
-
-export {asyncHandler}
