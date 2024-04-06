ecb4a01fbd51b803d7b9a0bda074a82afe59aa40
morningstar <mandeep7yadav@gmail.com>
2024-04-06 15:38:21 +0530

    responsive left

--- frontend/src/pages/signout.tsx
+++ frontend/src/pages/signout.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import { Link } from 'lucide-react'
+import React from 'react'
 
-const Signin = () => {
+const signout = () => {
   return (
+    <>
     <div className="flex justify-end w-full h-full mt-5 pr-10">
       <div className="w-[30%] shadow-lg py-8 px-6">
         <p className='text-sm font-semibold'>Welcome to <span className='text-blue-500'>Abhyudaya Club Management site</span></p>
-        <h1 className='text-[3rem] pl-2 font-[500]'>Sign in</h1>
-        <button className='w-full bg-blue-50 py-2 rounded-[10px] mt-6 justify-center text-xs text-blue-500 flex items-center gap-3'>
-          <span><img className='w-5 h-5' src="https://cdn.iconscout.com/icon/free/png-256/free-google-160-189824.png" alt="" /></span>Sign in with Google
-        </button>
+        <h1 className='text-[3rem] pl-2 font-[500]'>Sign up</h1>
 
-        <div className='mt-7'>
+        <div className='mt-5'>
           <p className='text-sm'>Enter your username or email address</p>
           <input type="text" placeholder='Username or email address'  className='w-full outline-none border border-blue-300 rounded-[5px] py-2 px-2 mt-2'/>
         </div>
+
+        <div className='mt-5 flex justify-between'>
+          <div>
+          <p className='text-sm'>User name</p>
+          <input type="text" placeholder='User name'  className='outline-none border border-blue-300 rounded-[5px] py-2 px-2 mt-2 w-[95%]'/>
+          </div>
+          <div>
+          <p className='text-sm'>Contact Number</p>
+          <input type="text" placeholder='Contact Number'  className='outline-none border border-blue-300 rounded-[5px] py-2 px-2 mt-2 w-[100%]'/>
+          </div>
+        </div>
+
         <div className='mt-5'>
           <p className='text-m'>Enter your Password</p>
           <input type="password" placeholder='Password'  className='w-full outline-none border border-blue-300 rounded-[5px] py-2 px-2 mt-2'/>
@@ -25,7 +36,8 @@ const Signin = () => {
         </div>
       </div>
     </div>
-  );
-};
+    </>
+  )
+}
 
-export default Signin;
+export default signout
