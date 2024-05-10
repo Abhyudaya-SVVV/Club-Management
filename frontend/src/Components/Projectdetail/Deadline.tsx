e361758fb4e67af0560345034d1defb08eb757c7
morningstar <mandeep7yadav@gmail.com>
2024-05-10 20:45:43 +0530

    [MY][DeadlineCardDone]

--- frontend/src/Components/Projectdetail/Deadline.tsx
+++ frontend/src/Components/Projectdetail/Deadline.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { SlCalender } from "react-icons/sl";
 
 const Deadline = () => {
   return (
     <>
-    <div className="w-[30%] h-[300px] m-5 shadow-lg rounded-[8px] overflow-y-auto">
+    <div className="w-[30%] h-[200px] m-5 shadow-lg rounded-[8px] overflow-y-auto">
                 <h1 className='pt-4 pl-4 text-xl'>Deadline</h1>
-                <h2 className='pt-1 pl-4 text-l text-zinc-600'>April 10,2024</h2>
+                <div className='flex items-center justify-center text-xl gap-10 mt-5'>
+                  <div>
+                  <div className='flex gap-2 items-center'><SlCalender color='#A2C3FB'/>From date</div>
+                  <h1 className='text-blue-500'>12 April 2024</h1>
+                  </div>
+
+                  <div>
+                  <div className='flex gap-2 items-center'><SlCalender/>To date</div>
+                  <h1>15 April 2024</h1>
+                  </div>
+                </div>
                 </div>
     </>
   )
